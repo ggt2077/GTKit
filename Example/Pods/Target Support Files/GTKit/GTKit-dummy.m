7e8d207cbdbc57e873fce75a34a74cf5aaff4b96
Tony <guoguangtao01@zhuanzhuan.com>
2018-10-25 20:03:05 +0800

    first commit

---
+++ Example/Pods/Target Support Files/GTKit/GTKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GTKit : NSObject
+@end
+@implementation PodsDummy_GTKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-GTKit_Example/Pods-GTKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GTKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GTKit_Example
+@end
